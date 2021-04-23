067e317638fabd7fa6220c8ab877e682a0da26ee
Ricardo Girardi <girardiricardo@outlook.com>
2021-04-23 17:19:59 -0300

    reconfigurando o storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,5 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-knobs/register"
-  ]
+  "addons": ["@storybook/addon-essentials"]
 }
